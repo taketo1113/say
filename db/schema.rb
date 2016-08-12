b57abd94270bd6a631a99f6f5362ec55df80d804
Taketo Takashima <t.taketo1113@gmail.com>
2016-08-13 02:52:52 +0900

    Fixed DEPRECATION WARNING of db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema.define(version: 20140822170751) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "body",       limit: 65535
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end
