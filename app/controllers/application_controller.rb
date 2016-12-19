848bee64f382ef2dfa031c6369ecc4d463ce09da
Taketo Takashima <t.taketo1113@gmail.com>
2016-12-19 17:28:05 +0900

    Added cors preflight check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   after_action :cors_set_access_control_headers
+  before_filter :cors_preflight_check
  
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+  end
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      render text: '', content_type: 'text/plain'
+    end
   end
 end
