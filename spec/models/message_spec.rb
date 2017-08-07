ac67b15a341728f939d4650507e79dbb7c1e4b87
Taketo Takashima <t.taketo1113@gmail.com>
2017-08-07 10:48:41 +0900

    Added model spec

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Message, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Message, type: :model do
+  before(:each) do
+    @message = Message.new(
+    )
+  end
+
+  it "is valid with valid attributes" do
+    expect(@message).to be_valid
+  end
 end
