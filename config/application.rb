6a33455e4f7c392d0165a2db7d79cbcd3a519fc0
Taketo Takashima <t.taketo1113@gmail.com>
2019-10-18 20:54:20 +0900

    Fixed eager_load_paths

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Say
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.autoload_paths += %W(#{config.root}/lib)
-#    config.eager_load_paths += %W(#{config.root}/lib)
+    config.eager_load_paths += %W(#{config.root}/lib)
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
