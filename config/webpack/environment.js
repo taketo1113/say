0368743168e0735b88660ca2ce4f561bcbd3e8e9
Taketo Takashima <t.taketo1113@gmail.com>
2019-11-10 10:21:00 +0900

    Added splitChunks of webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,12 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
 
+environment.splitChunks((config) => Object.assign({}, config, {
+  optimization: {
+    splitChunks: { name: 'vendor' }
+  }
+}))
+
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery'
