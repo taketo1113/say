6a7c37d4cd255cdd081612b62fca2f0d431f682b
Taketo Takashima <t.taketo1113@gmail.com>
2021-04-26 09:44:20 +0900

    Added to output ruby deprecated warning

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # for ruby deprecated warning
+  Warning[:deprecated] = true
 end
