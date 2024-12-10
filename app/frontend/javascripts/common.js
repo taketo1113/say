bc957fd4c4aafb0479f2c24c8dac2b391e3e0e1c
Taketo Takashima <t.taketo1113@gmail.com>
2024-12-10 09:52:59 +0900

    Replace from @rails/ujs to alt-ujs

--- app/frontend/javascripts/common.js
+++ app/frontend/javascripts/common.js
@@ -3,12 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-//import * as ActiveStorage from "@rails/activestorage"
-//import "channels"
+import AltUjs from "alt-ujs"
+AltUjs.start()
 
-Rails.start()
+//import * as ActiveStorage from "@rails/activestorage"
 //ActiveStorage.start()
+
+//import "channels"
 
 // lib
 import "bootstrap"
